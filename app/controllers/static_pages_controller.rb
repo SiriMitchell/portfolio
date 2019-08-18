546329a9ebc8e92765fcfe5fc80b883e467c6251
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-08-18 19:24:43 +0000

    changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,19 +3,4 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  def about
-  end
-
-  def skills
-  end
-
-  def projects
-  end
-
-  def contact
-  end
-
-  def resume
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  get 'about', to: 'static_pages#about'
-  get 'skills', to: 'static_pages#skills'
-  get 'projects', to: 'static_pages#projects'
-  get 'contact', to: 'static_pages#contact'
-  get 'resume', to: 'static_pages#resume'
 end
