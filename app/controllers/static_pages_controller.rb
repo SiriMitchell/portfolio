b6ee05528813770c40b5bdb611497b0fcce2c577
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-08-22 20:22:57 +0000

    added additional pages, links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,13 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def skills
+  end
+
+  def projects
+  end
+
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'skills', to: 'static_pages#skills'
+  get 'projects', to: 'static_pages#projects'
+  get 'about', to: 'static_pages#about'
 end
